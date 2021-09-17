9e7a1a5a83546c644abc8cb51b66ab6808599abf
Kolby Rogers <kolbyrogers@kAir.local>
2021-09-17 10:34:15 -0600

    fixed inquisitor

---
+++ src/inquisitors.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "image_menu.h"
+
+int main() {
+    // This function should call inquisitor, passing in std::cin and std::cout
+    // This function should return what inquisitor returns.
+    return inquisitor(std::cin, std::cout);
+}
