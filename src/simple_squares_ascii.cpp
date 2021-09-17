613aaf8efaa5293561d32d85eb07a53eb8d892de
Kolby Rogers <kolbyrogers@kAir.local>
2021-09-17 10:32:22 -0600

    added simple square

---
+++ src/simple_squares_ascii.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "image_menu.h"
+
+int main() {
+   return simple_squares_ascii(std::cin, std::cout);
+} 
+// This function should call simple_squares_ascii, passing in std::cin and std::cout
+//  This function should return what simple_squares_ascii returns.
